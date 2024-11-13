0f6a66f4ee5046b574d4630502b574bf00c1daf6
Jakub Turowski <turek979@gmail.com>
2024-11-13 20:21:32 +0100

    minor fixes

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ function rqListener(request, response){
     if(url === '/message' && method === 'POST'){
         const body = [];
         request.on('data', (chunk) =>{
-            console.log(chunk);
+            // console.log(chunk);
             body.push(chunk);
         });
-        request.on('end', () => {
+        return request.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
+            // console.log(parsedBody);
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            return response.end();
         });
-        response.statusCode = 302;
-        response.setHeader('Location', '/');
-        return response.end();
     }
     response.setHeader('Content-Type', 'text/html');
     response.write('<html>');

eab78d5d0901926793aec0d343461f0fed8bcd38
Jakub Turowski <turek979@gmail.com>
2024-11-13 19:11:48 +0100

    first html response

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 function rqListener(request, response){
-    console.log(request);
+    console.log(request.url, request.method, request.headers);
+    // process.exit();
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>My first Node.js Page</title></head>');
+    response.write('<body><h1>Hello World!</h1></body>');
+    response.write('</html>');
+    response.end();
 }
 
 const server = http.createServer(rqListener);

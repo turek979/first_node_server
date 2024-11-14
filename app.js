72260b8e1258cbf662cd054aa47cfd0d750a9c3b
J.Turowski <turek.priv@gmail.com>
2024-11-14 13:26:29 +0100

    improve fs function

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-function rqListener(request, response){
-    const url = request.url;
-    const method = request.method;
-    if(url === '/') {
-        response.write('<html>');
-        response.write('<head><title>Enter Message</title></head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        response.write('</html>');
+function rqListener(request, response) {
+  const url = request.url;
+  const method = request.method;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head><title>Enter Message</title></head>");
+    response.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    response.write("</html>");
+    return response.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      // console.log(chunk);
+      body.push(chunk);
+    });
+    return request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      // console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (error) => {
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
         return response.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        request.on('data', (chunk) =>{
-            // console.log(chunk);
-            body.push(chunk);
-        });
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            // console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            response.statusCode = 302;
-            response.setHeader('Location', '/');
-            return response.end();
-        });
-    }
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<html>');
-    response.write('<head><title>My first Node.js Page</title></head>');
-    response.write('<body><h1>Hello World!</h1></body>');
-    response.write('</html>');
-    response.end();
+      });
+    });
+  }
+  response.setHeader("Content-Type", "text/html");
+  response.write("<html>");
+  response.write("<head><title>My first Node.js Page</title></head>");
+  response.write("<body><h1>Hello World!</h1></body>");
+  response.write("</html>");
+  response.end();
 }
 
 const server = http.createServer(rqListener);
